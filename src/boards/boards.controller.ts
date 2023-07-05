eae676062f4c17723931f648a793d905f82cf445
Jungmin Kim <jmkimm00@icloud.com>
2023-07-05 14:47:31 +0900

    create board service

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,14 @@
 import { Controller } from '@nestjs/common';
+import { BoardsService } from './boards.service'; 
 
 @Controller('boards')
-export class BoardsController {}
+export class BoardsController {
+    constructor(private boardsService : BoardsService) {} // dependency injection
+} // private -> 접근제한자를 파라미터에 선언하면 접근 제한자가 사용된 생성자 파라미터는 클래스 프로퍼티로 선언이 된다.
+
+// export class BoardsController {
+//     boardsService: BoardsService;
+//     constructor(boardsService : BoardsService) {
+//         this.boardsService = boardsService;
+//     } // dependency injection
+// }
---
+++ src/boards/boards.service.ts
@@ -0,0 +1,6 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class BoardsService {
+    private boards = []; //다른 컴포넌트에서 수정하는 것을 막기 위해 private
+}
