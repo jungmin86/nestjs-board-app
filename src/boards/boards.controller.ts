0aa72b47229b3f6bf06dc5ea72caef98c13cf305
Jungmin Kim <jmkimm00@icloud.com>
2023-07-05 15:01:25 +0900

    Creating a Service to Fetch All Posts

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,13 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { BoardsService } from './boards.service'; 
 
-@Controller('boards')
+@Controller('boards') //localhost:3000/boards
 export class BoardsController {
-    constructor(private boardsService : BoardsService) {} // dependency injection
+    constructor(private boardsService: BoardsService) {} 
+    
+
+    @Get('/') //localhost:3000/boards (이게 끝)
+    getAllBoard() {
+        return this.boardsService.getAllBoards();
+    }
+    
 } // private -> 접근제한자를 파라미터에 선언하면 접근 제한자가 사용된 생성자 파라미터는 클래스 프로퍼티로 선언이 된다.
+// dependency injection
 
 // export class BoardsController {
-//     boardsService: BoardsService;
+//     boardsService: BoardsService; //선언부 (C+과 비슷)
 //     constructor(boardsService : BoardsService) {
 //         this.boardsService = boardsService;
 //     } // dependency injection
