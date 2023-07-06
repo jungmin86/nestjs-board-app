c511289a8521eca7cc7ab0afde51055a2e9f4669
Jungmin Kim <jmkimm00@icloud.com>
2023-07-06 23:33:27 +0900

    DTO for Creating a Board

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Board, BoardStatus } from './board.model';
 import { v1 as uuid } from 'uuid';
+import { CreateBoardDTO } from './dto/create-board.dto';
 
 @Injectable()
 export class BoardsService {
@@ -10,7 +11,8 @@ export class BoardsService {
         return this.boards;
     }
 
-    createBoard(title: string, description: string) {
+    createBoard(createBoardDTO: CreateBoardDTO) {
+        const {title, description} = createBoardDTO;
         const board: Board = {
             // title: title,
             // description: description,
