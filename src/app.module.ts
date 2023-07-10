76fa0fd3e80ca1ab8e1b48d93fe82e577a0b97ad
Jungmin Kim <jmkimm00@icloud.com>
2023-07-10 20:53:38 +0900

    TypeORM & MySql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { BoardsModule } from './boards/boards.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeORMConfig } from './configs/typeorm.config';
 
 
 @Module({
-  imports: [BoardsModule],
+  imports: [
+    BoardsModule,
+    TypeOrmModule.forRoot(typeORMConfig)
+  ],
 
 })
 export class AppModule {}
---
+++ src/boards/board.entity.ts
@@ -0,0 +1,18 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { BoardStatus } from "./board.model";
+
+@Entity()
+export class Board extends BaseEntity {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    title: string;
+
+    @Column()
+    description: string;
+
+    @Column()
+    status: BoardStatus;
+}
