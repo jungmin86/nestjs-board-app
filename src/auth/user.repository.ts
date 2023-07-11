785948ae6b90e4250a0060d8e69b198a638923a2
Jungmin Kim <jmkimm00@icloud.com>
2023-07-11 13:33:09 +0900

    Data Validation for Unique Username

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { ConflictException, Injectable, InternalServerErrorException } from "@nestjs/common";
 import { Repository, DataSource } from "typeorm";
 import { User } from "./user.entity";
 import { AuthCredentialDTO } from "./dto/auth-credential.dto";
@@ -13,8 +13,18 @@ export class UserRepository extends Repository<User> {
       async createUser(authCredentialDTO: AuthCredentialDTO): Promise<void> {
         const {username, password} = authCredentialDTO;
         const user = this.create({ username, password });
+        try {
+            await this.save(user);
+        } catch(error) {
+            console.log(error);
+            if(error.code === 'ER_DUP_ENTRY') {
+                throw new ConflictException('Existing username');
+            } else {
+                throw new InternalServerErrorException();
+            }
+
+        }
         
-        await this.save(user);
       }
 
 }
