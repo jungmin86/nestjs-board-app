18faec6a06b48438b235daba512977b9238147f4
Jungmin Kim <jmkimm00@icloud.com>
2023-07-11 13:56:19 +0900

    Password Encryption

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -2,6 +2,9 @@ import { ConflictException, Injectable, InternalServerErrorException } from "@ne
 import { Repository, DataSource } from "typeorm";
 import { User } from "./user.entity";
 import { AuthCredentialDTO } from "./dto/auth-credential.dto";
+import * as bcrypt from 'bcryptjs';
+
+
 
 @Injectable()
 export class UserRepository extends Repository<User> {
@@ -12,7 +15,10 @@ export class UserRepository extends Repository<User> {
 
       async createUser(authCredentialDTO: AuthCredentialDTO): Promise<void> {
         const {username, password} = authCredentialDTO;
-        const user = this.create({ username, password });
+        
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(password, salt);
+        const user = this.create({ username, password: hashedPassword });
         try {
             await this.save(user);
         } catch(error) {
